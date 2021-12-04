97be04c36e03fafd0a053307faaa549e215532a2
6nf-3f <93940370+6nf-3f@users.noreply.github.com>
2021-12-03 18:15:22 -1000

    Update part3.py

--- part3.py
+++ part3.py
@@ -20,3 +20,44 @@ not divisible by 3
 '''
 
 #write your code below
+number = int(input('Enter a number:' ))
+
+if number == 0:
+  print('zero')
+elif number > 0:
+  print('positive')
+else:
+  print('negative')
+
+if number % 3:
+  print('not divisible by 3')
+else:
+  print('divisible by 3')
+
+number = int(input('Enter a number:' ))
+
+if number == 0:
+  print('zero')
+elif number > 0:
+  print('positive')
+else:
+  print('negative')
+
+if number % 3:
+  print('not divisible by 3')
+else:
+  print('divisible by 3')
+
+number = int(input('Enter a number:' ))
+
+if number == 0:
+  print('zero')
+elif number > 0:
+  print('positive')
+else:
+  print('negative')
+
+if number % 3:
+  print('not divisible by 3')
+else:
+  print('divisible by 3')
